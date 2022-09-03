63ffe5435296a59bca5865b53e1a040577b05e04
Oleg Kaminskyy <olegkaminskyy@gmail.com>
2022-09-03 15:25:31 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Notify } from 'notiflix';
-import imageCards from './templates/image-card.hbs';
+// import imageCards from './templates/image-card.hbs';
 
 const KEY = '29675773-14c39cfbd09e94e65f3c5c74b';
 const BASE_URL = 'https://pixabay.com/api/';
